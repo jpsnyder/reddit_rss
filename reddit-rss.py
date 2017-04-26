6f9ee92b68c603e271bc74a1eea22701b8f2fb66
Jonathan Snyder <jonsnyder1@gmail.com>
2017-04-25 23:11:30 -0400

    Add entries.

--- reddit-rss.py
+++ reddit-rss.py
@@ -20,10 +20,28 @@ def produce_feed(subreddit='frontpage'):
     rss_url = 'http://reddit.com/r/{}.rss'.format(subreddit)
     feed = feedparser.parse(rss_url)
     cached_feed = PyRSS2Gen.RSS2(
+        generator=u'reddit-rss.com',
+        docs=u'github.com/jpsnyder/reddit-rss',
         title=feed['feed'].get('title'),
         link=feed['feed'].get('link'),
         description=feed['feed'].get('description'),
         lastBuildDate=feed['feed'].get('updated'))
+
+    for entry in feed['entries']:
+        # TODO: Add extra info: 'author_detail',
+        cached_entry = PyRSS2Gen.RSSItem(
+            guid=entry['id'],
+            title=entry['title'],
+            link=entry['link'],
+            description=entry['description'],
+            author=entry['author'],
+            # summary=entry['summary'],
+            pubDate=entry['updated'],
+            comments=None,
+        )
+        cached_feed.items.append(cached_entry)
+
+
     # TODO: Is there a way to avoid the StringIO nonsense?
     output = io.StringIO()
     cached_feed.write_xml(output)
