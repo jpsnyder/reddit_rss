7f0b262de4068944078eb28b6c41167d9b3b940c
Jonathan Snyder <jonsnyder1@gmail.com>
2017-04-27 21:23:23 -0400

    Switch to using reddit API

--- reddit-rss.py
+++ reddit-rss.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3.5
 
+import datetime
 import io
 import feedparser
 import flask
+import flask_cache
 import PyRSS2Gen
+import requests
 
 app = flask.Flask(__name__)
+# Cache resulting rss feed so we don't spam reddit with too many requests.
+cache = flask_cache.Cache(app, config={'CACHE_TYPE': 'simple'})
 
 
 def parse_rss_feed(rss_url):
@@ -14,35 +19,64 @@ def parse_rss_feed(rss_url):
 
 
 @app.route('/')
-@app.route('/<subreddit>')
 @app.route('/r/<subreddit>')
+@cache.memoize(timeout=60)
 def produce_feed(subreddit='frontpage'):
-    rss_url = 'http://reddit.com/r/{}.rss'.format(subreddit)
-    feed = feedparser.parse(rss_url)
+    json_url = 'http://reddit.com/r/{}.json'.format(subreddit)
+    response = requests.get(json_url)
+    if response.status_code != 200:
+        flask.abort(response.status_code)
+
+    feed = response.json()
+
     cached_feed = PyRSS2Gen.RSS2(
-        generator=u'reddit-rss.com',
-        docs=u'github.com/jpsnyder/reddit-rss',
-        title=feed['feed'].get('title'),
-        link=feed['feed'].get('link'),
-        description=feed['feed'].get('description'),
-        lastBuildDate=feed['feed'].get('updated'))
+        generator='reddit-rss.com',
+        docs='github.com/jpsnyder/reddit-rss',
+        title='TODO: {}'.format(subreddit),
+        link='TODO: {}'.format(subreddit),
+        description=None,
+        lastBuildDate=None,
+    )
 
-    for entry in feed['entries']:
-        # TODO: Add extra info: 'author_detail',
-        cached_entry = PyRSS2Gen.RSSItem(
-            guid=entry['id'],
-            title=entry['title'],
-            link=entry['link'],
-            description=entry['description'],
-            author=entry['author'],
-            # summary=entry['summary'],
-            pubDate=entry['updated'],
-            comments=None,
-        )
-        cached_feed.items.append(cached_entry)
+    for item in feed['data']['children']:
+        item = item['data']
+
+        cached_item = PyRSS2Gen.RSSItem(
+            guid=u'https://www.reddit.com/{}'.format(item['permalink']),
+            title=item['title'],
+            link=item['permalink'],
+            description=flask.render_template('rss_item.html', item=item),
+            author=item['author'],
+            pubDate=datetime.datetime.utcfromtimestamp(item['created_utc']))
+            # thumbnail=item['thumbnail'])
+        cached_feed.items.append(cached_item)
 
 
-    # TODO: Is there a way to avoid the StringIO nonsense?
+    # rss_url = 'http://reddit.com/r/{}.rss'.format(subreddit)
+    # feed = feedparser.parse(rss_url)
+    # cached_feed = PyRSS2Gen.RSS2(
+    #     generator=u'reddit-rss.com',
+    #     docs=u'github.com/jpsnyder/reddit-rss',
+    #     title=feed['feed'].get('title'),
+    #     link=feed['feed'].get('link'),
+    #     description=feed['feed'].get('description'),
+    #     lastBuildDate=feed['feed'].get('updated'))
+    #
+    # for entry in feed['entries']:
+    #     # TODO: Add extra info: 'author_detail',
+    #     cached_entry = PyRSS2Gen.RSSItem(
+    #         guid=entry['id'],
+    #         title=entry['title'],
+    #         link=entry['link'],
+    #         description=entry['description'],
+    #         author=entry['author'],
+    #         # summary=entry['summary'],
+    #         pubDate=entry['updated'],
+    #         comments=None,
+    #     )
+    #     cached_feed.items.append(cached_entry)
+
+
     output = io.StringIO()
     cached_feed.write_xml(output)
     output.seek(0)
